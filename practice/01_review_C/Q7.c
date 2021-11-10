e2d13551a42dd540c399f0c8a8b8f76743705a3c
Eesun <55704325+MoonEeSun@users.noreply.github.com>
2021-11-10 14:16:33 +0900

    review C_7

---
+++ practice/01_review_C/Q7.c
@@ -0,0 +1,22 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	int n, *px, *py, i, *p;
+	scanf("%d", &n);
+	px = (int*)malloc(n * sizeof(int));
+	py = (int*)malloc(n * sizeof(int));
+	for (p = px; p < px + n; p++)
+		scanf("%d", p);
+	for (p = py; p < py + n; p++)
+		scanf("%d", p);
+	for (i = 0; i < n; i++) {
+		printf(" %d", *(px + i) + *(py + n - 1 - i));
+	}
+
+	free(px);
+	free(py);
+	return 0;
+}
