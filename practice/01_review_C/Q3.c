ff637eb2369df2e1570aba081862bb6f75d7bd55
Eesun <55704325+MoonEeSun@users.noreply.github.com>
2021-11-10 14:14:33 +0900

    review C_3

---
+++ practice/01_review_C/Q3.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void swap(int *px, int *py);
+
+int main() {
+	int n, x[50], a, b, i;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &x[i]);
+	scanf("%d %d", &a, &b);
+	swap(&x[a], &x[b]);
+	for (i = 0; i < n; i++)
+		printf(" %d", x[i]);
+	return 0;
+}
+
+void swap(int *px, int *py) {
+	int tmp;
+	tmp = *px;
+	*px = *py;
+	*py = tmp;
+}
